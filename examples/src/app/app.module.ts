2a1c2d33b7a55ee43466f000dfe98cdeefb62ce3
Emir Basic <emir.basic@skatteverket.se>
2020-07-21 10:02:16 +0200

    Modifed gitignore

--- examples/src/app/app.module.ts
+++ examples/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-// import { MessagingModule as RunModule } from './messaging/messaging.module';
+import { MessagingModule as RunModule } from './messaging/messaging.module';
 // import { TodoModule as RunModule } from './todo/todo.module';
 // import { FormsDynamicModule as RunModule } from './forms-dynamic/forms-dynamic.module';
 // import { TestklientModule as RunModule } from './testklient/testklient.module';
-import { FormlyStartModule as RunModule } from './formly/formly-start.module';
+// import { FormlyStartModule as RunModule } from './formly/formly-start.module';
 
 @NgModule({
   declarations: [
